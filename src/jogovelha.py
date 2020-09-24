3d063306115e0b90973762bcc32f8a86d5a7d411
Stephaniemascarenhas <stephanie.souza@aluno.faculdadeimpacta.com.br>
2020-09-24 16:14:48 -0300

    ac2

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,8 +3,8 @@ def inicializar():
 	for i in range(3):
 		linha = [ ]
 		for j in range(3):
-			linha.append(".")
-		tab.ap pend(linha)
+			linha.ap pend("X")
+		tab.append(linha)
 	return tab
 
 def main( ):
